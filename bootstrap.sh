aa89ca6859f1fd722e134826778a33d8c90c0a4a
蔡涛 <caitao@zhejianglab.com>
2023-12-05 11:23:08 +0800

    修复bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,5 @@
 set -euo pipefail
 
 export PYTHONPATH=/home/cnife/code/convert_precomputed_web/src
-conda activate convert-precomputed-web
 
-uvicorn convert_precomputed_web.main:app --host 0.0.0.0 --port 6000
+/home/cnife/.miniconda3/envs/convert-precomputed-web/bin/python -m uvicorn convert_precomputed_web.main:app --host 0.0.0.0 --port 6000
